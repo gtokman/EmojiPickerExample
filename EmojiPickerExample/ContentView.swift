18664a41baaa10878fa1ac7cd55bec0b62432e57
Gary Tokman <hello@garytokman.me>
2021-04-14 09:39:54 -0400

    Initial Commit

---
+++ EmojiPickerExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EmojiPickerExample
+//
+//  Created by Gary Tokman on 4/14/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ EmojiPickerExample/EmojiPickerExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiPickerExampleApp.swift
+//  EmojiPickerExample
+//
+//  Created by Gary Tokman on 4/14/21.
+//
+
+import SwiftUI
+
+@main
+struct EmojiPickerExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
